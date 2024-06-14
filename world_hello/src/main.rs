a6ca736f3b431175b907e33d4e974a46cb0e7624
fi-dev-tang <3781768154@qq.com>
2024-06-14 15:08:09 +0800

    rust using println! as macro function

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn greet_world(){
+    let southern_gremany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let regions = [southern_gremany, chinese, english];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
+fn main(){
+    greet_world();
 }
